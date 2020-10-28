3e27dcec6c75c002e8740dba3e31a58f3fb30914
Alexander Giammaruti <44341602+Alexander-Giammaruti@users.noreply.github.com>
2020-10-27 21:39:40 -0500

    playing around with component lifecycle hooks

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,21 +8,43 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output('srvCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
-  newServerName = '';
-  newServerContent='';
+  // newServerName = '';
+  // newServerContent='';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
+  //COMPONENT LIFECYCLE BABY
 
-  constructor() { }
+  constructor() { } // runs at creation of the component
 
+  ngOnInit(): void { // runs after the constructor
+  }
 
-  ngOnInit(): void {
+  ngDoCheck() { // called during every change detection run
+  }
+
+  ngOnChanges() { // called after a bound input property changes
+  }
+  
+  ngAfterContentInit() { // called after content (ng-content) has been projected into view
+  }
+
+  ngAfterContentChecked() { // called every time the projected content has been checked
+  }
+
+  ngAfterViewInit() { // called after the component's view (and child views) have been initialized
+  }
+
+  ngAfterViewChecked() { //called every time the view (and child views) have been checked
+  }
+
+  ngOnDestroy() { // called right before the component is about to be destroyed
   }
 
   onAddServer(nameInput: HTMLInputElement){
-    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(nameInput: HTMLInputElement){
-    this.blueprintCreated.emit({blueprintName: nameInput.value, blueprintContent: this.newServerContent});
+    this.blueprintCreated.emit({blueprintName: nameInput.value, blueprintContent: this.serverContentInput.nativeElement.value});
   }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -9,9 +9,41 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string}; //@Input() decorator used to expose this property to the parent components which implement server-element-component allowing for binding to this property
                                                                                // 'srvElement' is an alias to allow for a different naming convention outside of this scope. now this element may only be referenced as 'srvElement' outside the scope of this file
-  constructor() { }
+  //COMPONENT LIFECYCLE BABY
 
-  ngOnInit(): void {
+  constructor() { // runs at creation of the component
+    console.log('constructor called');
+   } 
+
+  ngOnInit(): void { // runs after the constructor
+    console.log('ngOnInit called');
   }
 
+  ngDoCheck() { // called during every change detection run
+    console.log('ngDoCheck called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) { // called after a bound input property changes
+    console.log('ngOnChanges called: ' + changes);
+  }
+  
+  ngAfterContentInit() { // called after content (ng-content) has been projected into view
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() { // called every time the projected content has been checked
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() { // called after the component's view (and child views) have been initialized
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() { //called every time the view (and child views) have been checked
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() { // called right before the component is about to be destroyed
+    console.log('ngDestroy called');
+  }
 }
